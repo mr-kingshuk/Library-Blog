7451564b8f80373a1fa5e4d5a642dc2182468ffe
Kingshuk Ghosh <92975866+weeeb-coder@users.noreply.github.com>
2022-06-10 11:08:22 +0530

    local server set

---
+++ script.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const {google} = require("googleapis");
+
+const app = express();
+
+app.get("/",(req,res) => {
+
+    
+    res.send("Hello World!");
+});
+
+app.listen(1337, (req,res) => console.log("running on 1337"));
