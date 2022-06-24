d52caaa8db23dc8c0e365ea6e7273e6c0c635da0
Kingshuk Ghosh <92975866+weeeb-coder@users.noreply.github.com>
2022-06-24 15:43:47 +0530

    added links

--- JS/script.js
+++ JS/script.js
@@ -7,13 +7,15 @@ fetch("https://script.google.com/macros/s/AKfycbytz042kTz7jER3XLGRwtIDiO2UCkjgJX
     .then(res => res.json())
     .then(data => {
 
-        console.log(data.content);
-
         ritem3.querySelector(".title").innerHTML = data.content[1][0];
         ritem3.querySelector(".desc").innerHTML = data.content[1][1];
+        ritem3.querySelector(".link").href = data.content[1][2];
+        clipText(ritem3.querySelector(".desc"),200);
 
         ritem4.querySelector(".title").innerHTML = data.content[2][0];
         ritem4.querySelector(".desc").innerHTML = data.content[2][1];
+        ritem4.querySelector(".link").href = data.content[2][2];
+        clipText(ritem4.querySelector(".desc"),200);
         
     });
 
@@ -22,8 +24,19 @@ fetch("https://script.google.com/macros/s/AKfycbxIktXPeNzLwGlHOAUghu0DFe4bLARP9S
     .then(data => {
         hitem1.querySelector(".title").innerHTML = data.content[1][0];
         hitem1.querySelector(".desc").innerHTML = data.content[1][1];
+        hitem1.querySelector(".link").href = data.content[1][2];
+        clipText(hitem1.querySelector(".desc"),200);
 
         hitem2.querySelector(".title").innerHTML = data.content[2][0];
         hitem2.querySelector(".desc").innerHTML = data.content[2][1];
+        hitem2.querySelector(".link").href = data.content[1][2];
+        clipText(hitem2.querySelector(".desc"),200);
         
     });    
+
+
+function clipText(div, length){
+    var i;
+    div.innerHTML = div.innerHTML.substring(0,length) + "...";
+    
+}    
