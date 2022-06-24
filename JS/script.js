a08b4698fe268e9ce5d7f1138ae175336cce725f
Kingshuk Ghosh <92975866+weeeb-coder@users.noreply.github.com>
2022-06-24 13:24:55 +0530

    added script file

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,9 @@
 const hitem1 = document.querySelector('.i1');
-const hitem2 = document.querySelector('i2');
+const hitem2 = document.querySelector('.i2');
 const ritem3 = document.querySelector('.i3');
-const ritem4 = document.querySelector('i4');
+const ritem4 = document.querySelector('.i4');
 
-fetch("https://script.google.com/macros/s/AKfycbzJ0YExNeUs3OOhJ9bsZGkTuDidmSEnvHtOIPHC_YCk_asYf-pHhlMUZKGTl60MHZw/exec")
+fetch("https://script.google.com/macros/s/AKfycbytz042kTz7jER3XLGRwtIDiO2UCkjgJXFs0CfbeC_kiErmSAcNYeFfeFUSdTJw0N-k/exec")
     .then(res => res.json())
     .then(data => {
 
@@ -17,13 +17,13 @@ fetch("https://script.google.com/macros/s/AKfycbzJ0YExNeUs3OOhJ9bsZGkTuDidmSEnvH
         
     });
 
-fetch("https://script.google.com/macros/s/AKfycbzHyn-cc57UYmpSm0wWPCUK44HBV3yUZ3xxuliO54JyIEHUg1CQ1SZdbEzdvbJZiv0p/exec")
+fetch("https://script.google.com/macros/s/AKfycbxIktXPeNzLwGlHOAUghu0DFe4bLARP9SR7Mg-Au1_3XCzbzvcJT-X_f1CE252tSfeX/exec")
     .then(res => res.json())
     .then(data => {
-        ritem1.querySelector(".title").innerHTML = data.content[1][0];
-        ritem1.querySelector(".desc").innerHTML = data.content[1][1];
+        hitem1.querySelector(".title").innerHTML = data.content[1][0];
+        hitem1.querySelector(".desc").innerHTML = data.content[1][1];
 
-        ritem2.querySelector(".title").innerHTML = data.content[2][0];
-        ritem2.querySelector(".desc").innerHTML = data.content[2][1];
+        hitem2.querySelector(".title").innerHTML = data.content[2][0];
+        hitem2.querySelector(".desc").innerHTML = data.content[2][1];
         
     });    
